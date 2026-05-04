090b45d8f2b420dbadf4283967a816b46ddb961c
Francesco Cassese <francescocassese9@gmail.com>
2026-05-04 17:57:16 +0200

    feat: aggiunti e ridimensionati campi imput

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Card from "./components/Card";
-import listaArticoliPartenza from "./data/listaArticoliBlog";
+import listaArticoliBlog from "./data/listaArticoliBlog";
 import { useState } from "react";
 
 function App() {
@@ -11,33 +11,32 @@ function App() {
       </div>
     </header>
     <main>
-      <section className="col-12 col-md-8 mb-5">
+      <section className="col-12 col-md-8 mb-5 mx-auto">
         <div className="card shadow-sm border-0 bg-light p-4">
           <h2 className="h4 mb-4 text-primary border-bottom pb-2">Nuovo Articolo</h2>
 
           <form>
             {/* Input Titolo */}
             <div className="mb-3">
-              <label className="form-label fw-semibold">Titolo dell'articolo
-                <input
-                  type="text"
-                  id="titolo"
-                  className="form-control form-control-lg"
-                  placeholder="Inserisci un titolo accattivante..."
-                />
-              </label>
+              <label htmlFor="titolo" className="form-label fw-semibold">Titolo dell'articolo</label>
+              <input
+                type="text"
+                id="titolo"
+                className="form-control form-control-lg"
+                placeholder="Inserisci un titolo accattivante..."
+              />
             </div>
 
             {/* Input Contenuto */}
             <div className="mb-4">
-              <label className="form-label fw-semibold">Contenuto del post
-                <textarea
-                  id="contenuto"
-                  className="form-control"
-                  rows="5"
-                  placeholder="Di cosa vuoi parlare oggi?"
-                ></textarea>
-              </label>
+              <label htmlFor="contenuto" className="form-label fw-semibold">Contenuto del post</label>
+              <textarea
+                id="contenuto"
+                className="form-control"
+                rows="5"
+                placeholder="Di cosa vuoi parlare oggi?"
+              ></textarea>
+
             </div>
 
             {/* Bottone di invio */}
@@ -52,7 +51,7 @@ function App() {
       <div className="container my-5">
         <div className="row">
           <div className="col-12">
-            <Card listaElements={listaArticoliPartenza} />
+            <Card listaElements={listaArticoliBlog} />
           </div>
         </div>
       </div>
