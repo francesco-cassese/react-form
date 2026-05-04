089357d66ef0354e30060978d9c8d6543f1d86b5
Francesco Cassese <francescocassese9@gmail.com>
2026-05-04 16:30:35 +0200

    feat: create e stampate le prime 5 card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Card from "./components/card";
+import listaProdotti from "./data/listaProdottiCard";
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div className="container my-5">
+      <div className="row">
+        <div className="col-12">
+          <Card listaElements={listaProdotti} />
+        </div>
+      </div>
+    </div>
   );
 }
 export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,23 @@
+function Card(props) {
+    const listaProdottiJsx = props.listaElements.map(protodotto => {
+        console.log(props.listaElements);
+        const { id, titolo, contenuto, urlImmagine } = protodotto
+        return (
+            <li key={id} className="col-12 col-md-4 p-2">
+                <div className="card">
+                    <img src={urlImmagine} className="card-img-top" alt={titolo} />
+                    <div className="card-body">
+                        <h5 className="card-title">{titolo}</h5>
+                        <p className="card-text">{contenuto}</p>
+                    </div>
+                </div>
+            </li>
+        )
+    })
+    return (
+        <ul className="list-unstyled d-flex flex-wrap">
+            {listaProdottiJsx}
+        </ul>
+    )
+}
+export default Card
